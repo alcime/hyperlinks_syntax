e42f97306e805dd2093d9cdd5e28d03786ed259e
root <root@localhost.localdomain>
2019-12-10 17:27:29 +0100

    precisions dans le read.ME, specialisation du scraper pour qu'il cible les pages du NYT.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # hyperlinks_syntax
-Projet d'analyse quantitative de la syntaxe des hyperliens
+Projet d'analyse quantitative de la syntaxe des hyperliens. Le but est de recuperer les hyperliens ainsi que leur destination pour analyser ensuite leur categorie grammaticale afin de voir si la majorite des hyperliens sont des syntagmes usuels en syntaxe.
+
 Ce projet se compose de deux programmes: un scraper et un analyzer.
 
-Le scraper, a partir d'une page HTML, recupere dans les colonnes d'un fichier CSV les donnees suivantes:
-1. Phrase dans laquelle il y a un hyper lien 2. partie du texte qui est hyperlinkee 3.destination de l'hyperlien.
+Je me suis charge de la partie scraper. (Louis-Marie)
+
+Le scraper, a partir d'une page HTML du Washington Post, recupere dans les colonnes d'un fichier TSV les donnees suivantes:
+1. partie du texte qui est hyperlinkee 
+2. destination de l'hyperlien.
+
+
+Contexte : Je n'avais jamais programme avant, meme si j'avais appris a utiliser le terminal, et j'ai commence Python cette annee.
+J'ai essaye d'integrer autant de notions que possibles (definition de fonction, Regex, ... ) dans mon programme, et surtout de le construire sans utiliser un parser (la library BeautifulSoup n'est utilisee que pour du formatage). Ceci a engendre plusieurs limitations : tout d'abord, le scraper est construit specialement pour des pages du Washington Post. De plus, il m'a fallu reconstruire certaines fonctions d'un parseur a la main, par exemple le formatage du texte que l'on scrape.
+
+
 
 L'analyzer, a partir d'un fichier csv [A completer]
--- Scrapper.py
+++ Scrapper.py
@@ -1,7 +1,39 @@
-""" Scrape les hyperliens d'une page html et les range dans un csv"""
+""" Scrape les hyperliens d'une page html du Washington Post et les range dans un csv"""
 
+
+# Libraries
 import pandas as pd
 from bs4 import BeautifulSoup
+import re
+from argparse import ArgumentParser
+
+# Variables
+tag_class = "a class=\"css-1g7m0tk\" href="
+which_parser = "lxml"
+print_in_console = True
+output_file_relative_pathname = 'scrapped_url.tsv'
+
+# Create a parser for the terminal
+#t_parser = ArgumentParser()
+
+#t_parser.add_argument("-f", "--file", dest="filename", help="Relative path of the html file you want to scrape ", default = 'html_simple.html')
+#t_parser.add_argument("-v+", "--verbose", action="store_true", dest="verbose", default=True, help="Print the results of intermediate steps in terminal")
+#t_parser.add_argument("-p", "--parser", dest="parser_type", default="lxml", help="Type of the parser")
+
+#args = t_parser.parse_args()
+
+input_file_name = args.filename
+which_parser = args.parser_type
+print_in_console = args.verbose
+
+
+# Functions
+def format_string (string):
+   """ Delete in a string : newlines, tabulations and leading space"""
+   string_formatted = string.replace('\n', '')
+   string_formatted = string_formatted.replace('\t', '')
+   string_formatted = re.sub(r"^\s+", "", string_formatted) # Remove leading spaces
+   return string_formatted
 
 
 # Ask in prompt the name of the file
@@ -9,10 +41,6 @@ input_file_name = input("Name of the file :")
 print ('\n')
 if input_file_name == '':
    input_file_name = 'html_simple.html'
-
-which_parser = "lxml"
-print_in_console = True
-output_file_relative_pathname = 'scrapped_url.tsv'
 
 
 # Open a tsv to write the data in it
@@ -33,10 +61,10 @@ with open(input_file_name, 'r') as html_file :
 
 while True: 
    # Extract the url
-   i_where_link_starts = pretty_soup.find("href=")
+   i_where_link_starts = pretty_soup.find(tag_class)
 
    if i_where_link_starts == -1:
-      print('Pas de liens')
+      print('No more links')
       break
 
    i_start_quote = pretty_soup.find('"', i_where_link_starts)
@@ -53,18 +81,25 @@ while True:
    i_end_hyperlinked = pretty_soup.find('</a>', i_start_hyperlinked + 1)
    hyperlinked = pretty_soup[i_start_hyperlinked + 1 : i_end_hyperlinked]
 
+   # Format the url and the hyperlinked
+   url_formatted = format_string(url)
+   hyperlinked_formatted = format_string(hyperlinked)
+   
    # Delete the part the Scraper already processed
    pretty_soup = pretty_soup[i_end_quote:]
 
    
 
-   # Check if the url is not part of a script
+   # Check approximately if the url is not part of a script (this is because when embedded in javascript code, double slash will be coded as '/\/\' in the html code source
    if url.find('\/\/') == -1 :
+      
       # Write the data in tsv
       if print_in_console :
-         print(":".join("{:02x}".format(ord(char)) for char in hyperlinked))
-         print(url + hyperlinked)
-      output_file.write(url + '\t' + hyperlinked)
+         #print(":".join("{:02x}".format(ord(char)) for char in hyperlinked_formatted)) Debugging lines to check the hexadecimal of the data
+         #print(":".join("{:02x}".format(ord(char)) for char in url_formatted))
+         print(url_formatted + '\t' +  hyperlinked_formatted)
+         #print (":".join("{:02x}".format(ord(char)) for char in url_formatted + hyperlinked_formatted))
+      output_file.write(url_formatted + '\t' + hyperlinked_formatted + '\n')
    
    
 
